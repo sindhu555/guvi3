f707d712796fff8f13fc11d4c138cc1cc6b8db7f
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 11:31:04 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -7,11 +7,11 @@ class Ideone
   {
     Scanner sc=new Scanner(System.in);
     char ch=sc.next().charAt(0);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
     {
       System.out.println("Vowel");
     }
-    else if (ch >= 'a' && ch <= 'z') 
+    else if (ch >= 'a' && ch <= 'z'||ch >= 'A' && ch <= 'Z') 
     {
       System.out.println("Consonant");
     }
