0b7554d81d2098a39af49e5933571c13eaf2a27f
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-07-27 11:11:25 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -1,1 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.Scanner;
+class Ideone
+{
+  public static void main(String args[]) throws java.lang.Exception
+  {
+    Scanner sc=new Scanner(System.in);
+    char ch=sc.next().charAt(0);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+      System.out.println("Vowel");
+    }
+    else if (ch >= 'a' && ch <= 'z') 
+    {
+      System.out.println("Constant");
+    }
+    else
+    {
+      System.out.println("invalid");
+     }
+   }
+  }
+     
 
