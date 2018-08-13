0a1c97ac633e575378bf494ad799d27eaf346b1b
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-13 12:55:52 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,count=0,j=1;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		j=j*2;
+	if(j==n)
+		count=1;
+	}
+	if(count==1)
+	{
+			printf("\n yes");
+	}
+	else
+		printf("\n no");
+
+}
