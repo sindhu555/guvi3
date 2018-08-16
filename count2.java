7e62d35d75416ab0fb78ce9778e7a01dacbbfb3d
sindhu555 <41734778+sindhu555@users.noreply.github.com>
2018-08-16 19:53:31 +0530

    Create count2.java

---
+++ count2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		 int count = 0, num = 3452;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println(  count);
+    }
+}
